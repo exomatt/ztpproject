37e5fda7547caefde19de796211eaf5a6cb4f503
Mateusz Pernal <exomat@wp.pl>
2019-01-10 17:38:32 +0100

    initialize Iterators class

--- src/main/java/iterator/AlphabetIterator.java
+++ src/main/java/iterator/AlphabetIterator.java
@@ -1,4 +1,17 @@
 package iterator;
 
-public class AlphabetIterator {
+import model.Word;
+
+import java.util.Iterator;
+
+public class AlphabetIterator implements Iterator<Word> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Word next() {
+        return null;
+    }
 }
--- src/main/java/iterator/RandomIterator.java
+++ src/main/java/iterator/RandomIterator.java
@@ -1,4 +1,17 @@
 package iterator;
 
-public class RandomIterator {
+import model.Word;
+
+import java.util.Iterator;
+
+public class RandomIterator implements Iterator<Word> {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Word next() {
+        return null;
+    }
 }

533a0d1882458e62e4e5bbba7e2524680df59c65
Mateusz Pernal <exomat@wp.pl>
2019-01-12 14:16:37 +0100

    implement iterators

--- src/main/java/iterator/AlphabetIterator.java
+++ src/main/java/iterator/AlphabetIterator.java
@@ -1,17 +1,40 @@
 package iterator;
 
+import lombok.extern.java.Log;
 import model.Word;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
+@Log
 public class AlphabetIterator implements Iterator<Word> {
+    List<Word> wordList;
+    int position;
+
+    public AlphabetIterator(List<Word> listFromDB, int number) {
+        position = 0;
+        if (listFromDB.size() > number) {
+            log.severe("Number is greater than list size");
+        }
+        Collections.shuffle(listFromDB);
+        for (int i = 0; i < number; i++) {
+            wordList.add(listFromDB.get(i));
+        }
+        Collections.sort(wordList, Comparator.comparing(Word::getWord));
+    }
+
     @Override
     public boolean hasNext() {
-        return false;
+        if (position == wordList.size()) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public Word next() {
-        return null;
+        return wordList.get(position++);
     }
 }
--- src/main/java/iterator/RandomIterator.java
+++ src/main/java/iterator/RandomIterator.java
@@ -2,16 +2,33 @@ package iterator;
 
 import model.Word;
 
+import java.util.Collections;
 import java.util.Iterator;
+import java.util.List;
 
 public class RandomIterator implements Iterator<Word> {
+    List<Word> wordList;
+    int position;
+
+    public RandomIterator(List<Word> listFromDB, int number) {
+        position = 0;
+        Collections.shuffle(listFromDB);
+        for (int i = 0; i < number; i++) {
+            wordList.add(listFromDB.get(i));
+        }
+        Collections.shuffle(wordList);
+    }
+
     @Override
     public boolean hasNext() {
-        return false;
+        if (position == wordList.size()) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public Word next() {
-        return null;
+        return wordList.get(position++);
     }
 }

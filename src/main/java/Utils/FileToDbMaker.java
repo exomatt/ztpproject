3d342f9ef3adc9b398ac5b8310454d82ed09be80
Mateusz Pernal <exomat@wp.pl>
2019-01-15 22:51:29 +0100

    add function to fill DB with words from file

---
+++ src/main/java/Utils/FileToDbMaker.java
@@ -0,0 +1,21 @@
+package Utils;
+
+import database.DatabaseEditor;
+import database.DatabaseRepository;
+import database.FileDatabase;
+import model.Word;
+
+import java.util.List;
+
+public class FileToDbMaker {
+    public static void main(String[] args) {
+        DatabaseEditor editorFile = new DatabaseEditor();
+        DatabaseEditor editorDB = new DatabaseEditor();
+        editorFile.setDatabase(new FileDatabase());
+        editorDB.setDatabase(new DatabaseRepository());
+        List<Word> wordList = editorFile.findByLanguage("pl");
+        for (Word word : wordList) {
+            editorDB.addWord(word);
+        }
+    }
+}
